4a3af5947e194f68bdfa750da6760fec313bc5d2
flatded <truebanger14@gmail.com>
2024-12-31 17:05:49 -0500

    $ sign has been lost

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ function Header() {
             <ul className="d-flex">
                 <li className="mr-30">
                     <img width={18} height={18} src="/img/cart.svg" alt="cart"/>
-                    <span>249.99</span>
+                    <span>$249.99</span>
                 </li>
                 <li>
                     <img width={18} height={18} src="/img/user.svg" alt="user"/>
