50f4b498165f50c0bdaf1692706a17f491b49baa
flatded <truebanger14@gmail.com>
2025-01-02 15:18:13 -0500

    fast fix

--- src/components/Drawer/Drawer.jsx
+++ src/components/Drawer/Drawer.jsx
@@ -1,11 +1,67 @@
 function Drawer() {
     return (
-        <div style={{display: "none"}} className="overlay">
+        <div className="overlay">
             <div className="drawer">
                 <h2 className="d-flex justify-between mb-40">Shopping cart <img className="cu-p" src="/img/remove.svg"
                                                                                 alt="remove" height={32} width={32}/>
                 </h2>
                 <div className="items">
+                    <div className="cartItems d-flex align-center mb-20">
+                        <div style={{backgroundImage: 'url(/img/sneakers/1.jpg)'}} className="cartItemsImg"></div>
+                        <div className="mr-20">
+                            <p className="mb-5">Men sneakers Rabook or Nikey version 44</p>
+                            <b>$159.99</b>
+                        </div>
+                        <img className="remove" src="/img/remove.svg" alt="remove" height={32} width={32}/>
+                    </div>
+                    <div className="cartItems d-flex align-center mb-20">
+                        <div style={{backgroundImage: 'url(/img/sneakers/1.jpg)'}} className="cartItemsImg"></div>
+                        <div className="mr-20">
+                            <p className="mb-5">Men sneakers Rabook or Nikey version 44</p>
+                            <b>$159.99</b>
+                        </div>
+                        <img className="remove" src="/img/remove.svg" alt="remove" height={32} width={32}/>
+                    </div>
+                    <div className="cartItems d-flex align-center mb-20">
+                        <div style={{backgroundImage: 'url(/img/sneakers/1.jpg)'}} className="cartItemsImg"></div>
+                        <div className="mr-20">
+                            <p className="mb-5">Men sneakers Rabook or Nikey version 44</p>
+                            <b>$159.99</b>
+                        </div>
+                        <img className="remove" src="/img/remove.svg" alt="remove" height={32} width={32}/>
+                    </div>
+                    <div className="cartItems d-flex align-center mb-20">
+                        <div style={{backgroundImage: 'url(/img/sneakers/1.jpg)'}} className="cartItemsImg"></div>
+                        <div className="mr-20">
+                            <p className="mb-5">Men sneakers Rabook or Nikey version 44</p>
+                            <b>$159.99</b>
+                        </div>
+                        <img className="remove" src="/img/remove.svg" alt="remove" height={32} width={32}/>
+                    </div>
+                    <div className="cartItems d-flex align-center mb-20">
+                        <div style={{backgroundImage: 'url(/img/sneakers/1.jpg)'}} className="cartItemsImg"></div>
+                        <div className="mr-20">
+                            <p className="mb-5">Men sneakers Rabook or Nikey version 44</p>
+                            <b>$159.99</b>
+                        </div>
+                        <img className="remove" src="/img/remove.svg" alt="remove" height={32} width={32}/>
+                    </div>
+                    <div className="cartItems d-flex align-center mb-20">
+                        <div style={{backgroundImage: 'url(/img/sneakers/1.jpg)'}} className="cartItemsImg"></div>
+                        <div className="mr-20">
+                            <p className="mb-5">Men sneakers Rabook or Nikey version 44</p>
+                            <b>$159.99</b>
+                        </div>
+                        <img className="remove" src="/img/remove.svg" alt="remove" height={32} width={32}/>
+                    </div>
+                    <div className="cartItems d-flex align-center mb-20">
+                        <div style={{backgroundImage: 'url(/img/sneakers/1.jpg)'}} className="cartItemsImg"></div>
+                        <div className="mr-20">
+                            <p className="mb-5">Men sneakers Rabook or Nikey version 44</p>
+                            <b>$159.99</b>
+                        </div>
+                        <img className="remove" src="/img/remove.svg" alt="remove" height={32} width={32}/>
+                    </div>
                     <div className="cartItems d-flex align-center mb-20">
                         <div style={{backgroundImage: 'url(/img/sneakers/1.jpg)'}} className="cartItemsImg"></div>
                         <div className="mr-20">
